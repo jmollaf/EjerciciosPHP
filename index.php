e7f78e1243c56849c71e04bfdea8c9a03000d5bf
jmollaf <jmollaf@gmail.com>
2015-12-04 13:23:48 +0100

    Ejercicio 1

---
+++ index.php
@@ -0,0 +1,38 @@
+<html>
+<?php
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+    <head>
+           <style type="text/css">
+                  div {text-align:center};
+           </style>    
+    </head>
+    <body>
+        <title>
+            EJERCICIOS PHP
+        </title>
+        <div> <h1>EJERCICIOS PHP</h1></div>
+        <div><a href="ejercicio1.php">EJERCICIO 1</a></div><br>
+        <div><a href="ejercicio2.php">EJERCICIO 2</a></div><br>
+        <div><a href="ejercicio3.php">EJERCICIO 3</a></div><br>
+        <div><a href="ejercicio4.php">EJERCICIO 4</a></div><br>
+        <div><a href="ejercicio5.php">EJERCICIO 5</a></div><br>
+        <div><a href="ejercicio6.php">EJERCICIO 6</a></div><br>
+        <div><a href="ejercicio7.php">EJERCICIO 7</a></div><br>
+        <div><a href="ejercicio8.php">EJERCICIO 8</a></div><br>
+        <div><a href="ejercicio9.php">EJERCICIO 9</a></div><br>
+        <div><a href="ejercicio10.php">EJERCICIO 10</a></div><br>
+        <div><a href="ejercicio11.php">EJERCICIO 11</a></div><br>
+        <div><a href="ejercicio12.php">EJERCICIO 12</a></div><br>
+        <div><a href="ejercicio13.php">EJERCICIO 13</a></div><br>
+        <div><a href="ejercicio14.php">EJERCICIO 14</a></div><br>
+        <div><a href="ejercicio15.php">EJERCICIO 15</a></div><br>
+        <div><a href="ejercicio16.php">EJERCICIO 16</a></div><br>
+        <div><a href="ejercicio17.php">EJERCICIO 17</a></div><br>
+        <div><a href="ejercicio18.php">EJERCICIO 18</a></div><br>
+    </body>
+</html>
